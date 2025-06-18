f7696ea747deedfed1c23613660e2c7c8c909b38
gowtham <gowtham2151@gmail.com>
2025-06-18 13:00:28 +0530

    updation of json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/E-Commerce-Product-Catalog/",  // ? replace with your actual repo name
   plugins: [react()],
 })
